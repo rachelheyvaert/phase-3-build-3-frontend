759f55ecbe42f2d39427ed3a3d33066dc8261937
Rachel Heyvaert <rachelheyvaert@gmail.com>
2022-08-07 14:59:56 -0400

    cleaned up and added some styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 
-import { Button } from '@material-ui/core';
+import { Button, makeStyles } from '@material-ui/core';
 
+const useStyles = makeStyles({
+  button: {
+    backgroundColor: 'violet'
+  }
+})
 const App = () => {
+  const classes = useStyles();
   return (
     <div className="App">
       <h1>Hello Rachel</h1>
-      <Button color="primary" variant="contained">click</Button>
+      <Button className={ classes.button } variant="contained">click</Button>
     </div>
   );
 }
